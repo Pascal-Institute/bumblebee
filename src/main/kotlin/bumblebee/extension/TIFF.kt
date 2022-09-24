b33104818201977780a14b9ec7893491004713f9
volta2030 <volta2030@gmail.com>
2022-09-25 08:54:57 +0900

    -Modify TIFF

--- src/main/kotlin/bumblebee/extension/TIFF.kt
+++ src/main/kotlin/bumblebee/extension/TIFF.kt
@@ -3,6 +3,7 @@ package bumblebee.extension
 import bumblebee.core.ImgPix
 import bumblebee.type.ColorType
 import bumblebee.type.ImgFileType
+import bumblebee.type.TagType
 import bumblebee.util.Converter.Companion.byteToHex
 import bumblebee.util.Converter.Companion.hexToInt
 import bumblebee.util.Converter.Companion.invert
@@ -13,13 +14,13 @@ class TIFF(private var byteArray: ByteArray) : ImgPix() {
 
     private var ifh = IFH()
     private var ifdArray = ArrayList<IFD>()
+
     init {
         imgFileType = if (byteArray.sliceArray(0 until 2).contentEquals(ImgFileType.TIFF_LITTLE.signature)){
             ImgFileType.TIFF_LITTLE
         }else{
             ImgFileType.TIFF_BIG
         }
-
         metaData.colorType = ColorType.TRUE_COLOR
         this.pixelBufferArray = ByteBuffer.allocate(0)
 
@@ -27,21 +28,21 @@ class TIFF(private var byteArray: ByteArray) : ImgPix() {
     }
 
     override fun extract() {
-        ifh.extract(ifdArray, byteArray)
+        ifh.extract(imgFileType, ifdArray, byteArray)
     }
 
     //Image File Header
-    private class IFH{
+    private class IFH {
         lateinit var byteOrder : ByteArray
         lateinit var fortyTwo : ByteArray
         lateinit var firstIFDOffset : ByteArray
 
-        fun extract(ifdArray: ArrayList<IFD>, byteArray: ByteArray){
+        fun extract(imgFileType: ImgFileType, ifdArray: ArrayList<IFD>, byteArray: ByteArray){
             byteOrder = byteArray.sliceArray(0 until 2)
             fortyTwo = byteArray.sliceArray(2 until 4)
             firstIFDOffset = byteArray.sliceArray(4 until 8)
 
-            ifdArray.add(IFD(byteArray.sliceArray(
+            ifdArray.add(IFD(imgFileType, byteArray.sliceArray(
                 hexToInt(byteToHex(invert(firstIFDOffset)))
                         until
                         byteArray.size)))
@@ -49,7 +50,7 @@ class TIFF(private var byteArray: ByteArray) : ImgPix() {
     }
 
     //Image File Directory
-    class IFD(byteArray: ByteArray){
+    private class IFD(imgFileType: ImgFileType, byteArray: ByteArray){
         var numOfTags : ByteArray = byteArray.sliceArray(0 until 2) //2 Byte
         var tagArray = ArrayList<Tag>() //12 Byte * numOfTags
         var nextIFDOffset : ByteArray //4 Byte
@@ -57,7 +58,7 @@ class TIFF(private var byteArray: ByteArray) : ImgPix() {
         init {
             var value = hexToInt(byteToHex(invert(numOfTags)))
             for(i : Int in 0 until  value){
-                tagArray.add(Tag(byteArray.sliceArray(2 + i*12 until 2 + (i+1) * 12)))
+                tagArray.add(Tag(imgFileType, byteArray.sliceArray(2 + i*12 until 2 + (i+1) * 12)))
             }
             nextIFDOffset = byteArray.sliceArray(2 + 12 * value until 2 + 12 * value + 4)
 
@@ -66,17 +67,26 @@ class TIFF(private var byteArray: ByteArray) : ImgPix() {
         }
     }
 
-    class Tag(byteArray: ByteArray) {
-        var tagId : ByteArray = byteArray.sliceArray(0 until 2) //2 Byte
+    private class Tag(imgFileType: ImgFileType, byteArray: ByteArray) {
+        var tagId : TagType = TagType.fromByteArray(
+            if(imgFileType.signature.contentEquals(ImgFileType.TIFF_LITTLE.signature)){
+                invert(byteArray.sliceArray(0 until 2))
+            }else{
+                byteArray.sliceArray(0 until 2)
+            }
+        )
+
         var dataType : ByteArray = byteArray.sliceArray(2 until 4) //2 Byte
         var dataCount : ByteArray = byteArray.sliceArray(4 until 8) //4 Byte
         var dataOffset : ByteArray = byteArray.sliceArray(8 until 12) // 4Byte
 
         init {
-//            println(byteToHex(invert(tagId)))
-//            println(byteToHex(invert(dataType)))
-//            println(byteToHex(invert(dataCount)))
-//            println(byteToHex(invert(dataOffset)))
+
+
+            println(tagId.name)
+            println(byteToHex(invert(dataType)))
+            println(byteToHex(invert(dataCount)))
+            println(byteToHex(invert(dataOffset)))
         }
 
     }
