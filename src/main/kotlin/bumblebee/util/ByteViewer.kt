f7c551b07d8f6ea0a301f7a8dd2dd92b619fb265
volta2030 <volta2030@gmail.com>
2022-10-09 23:23:13 +0900

    -Modify

--- src/main/kotlin/bumblebee/util/ByteViewer.kt
+++ src/main/kotlin/bumblebee/util/ByteViewer.kt
@@ -1,10 +1,18 @@
 package bumblebee.util
 
 import bumblebee.util.Converter.Companion.byteToHex
-import javax.swing.JFrame
-import javax.swing.JScrollPane
-import javax.swing.JTable
-import javax.swing.WindowConstants
+import java.awt.Component
+import java.awt.Font
+import java.beans.PropertyChangeEvent
+import java.beans.PropertyChangeListener
+import javax.swing.*
+import javax.swing.event.ChangeEvent
+import javax.swing.event.ChangeListener
+import javax.swing.event.TableModelEvent
+import javax.swing.event.TableModelListener
+import javax.swing.table.DefaultTableCellRenderer
+import javax.swing.table.JTableHeader
+import javax.swing.table.TableColumn
 
 
 class ByteViewer(val byteArray : ByteArray) : JFrame() {
@@ -21,13 +29,19 @@ class ByteViewer(val byteArray : ByteArray) : JFrame() {
     }
 
     private fun build() {
+
         extract()
+
         table = JTable()
 
         val header = arrayOf("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E")
+
         val contents = extract()
         val table = JTable(contents, header)
+        val rowTable =  RowNumberTable(table)
+
         val scrollPane = JScrollPane(table)
+        scrollPane.setRowHeaderView(rowTable)
 
         this.add(scrollPane)
     }
@@ -46,6 +60,131 @@ class ByteViewer(val byteArray : ByteArray) : JFrame() {
         }
 
         return array
+    }
 
+    // Reference from http://www.camick.com/java/source/RowNumberTable.java
+    class RowNumberTable(private val main: JTable) : JTable(), ChangeListener, PropertyChangeListener,
+        TableModelListener {
+        init {
+            main.addPropertyChangeListener(this)
+            main.model.addTableModelListener(this)
+            isFocusable = false
+            setAutoCreateColumnsFromModel(false)
+            setSelectionModel(main.selectionModel)
+            val column = TableColumn()
+            column.headerValue = " "
+            addColumn(column)
+            column.cellRenderer = RowNumberRenderer()
+            getColumnModel().getColumn(0).preferredWidth = 50
+            preferredScrollableViewportSize = preferredSize
+        }
+
+        override fun addNotify() {
+            super.addNotify()
+            val c: Component = parent
+
+            //  Keep scrolling of the row table in sync with the main table.
+            if (c is JViewport) {
+                val viewport: JViewport = c
+                viewport.addChangeListener(this)
+            }
+        }
+
+        /*
+	 *  Delegate method to main table
+	 */
+        override fun getRowCount(): Int {
+            return main.rowCount
+        }
+
+        override fun getRowHeight(row: Int): Int {
+            val rowHeight = main.getRowHeight(row)
+            if (rowHeight != super.getRowHeight(row)) {
+                super.setRowHeight(row, rowHeight)
+            }
+            return rowHeight
+        }
+
+        /*
+	 *  No model is being used for this table so just use the row number
+	 *  as the value of the cell.
+	 */
+        override fun getValueAt(row: Int, column: Int): Any {
+            return (row + 1).toString()
+        }
+
+        /*
+	 *  Don't edit data in the main TableModel by mistake
+	 */
+        override fun isCellEditable(row: Int, column: Int): Boolean {
+            return false
+        }
+
+        /*
+	 *  Do nothing since the table ignores the model
+	 */
+        override fun setValueAt(value: Any, row: Int, column: Int) {}
+
+        //
+        //  Implement the ChangeListener
+        //
+        override fun stateChanged(e: ChangeEvent) {
+            //  Keep the scrolling of the row table in sync with main table
+            val viewport: JViewport = e.source as JViewport
+            val scrollPane = viewport.parent as JScrollPane
+            scrollPane.verticalScrollBar.value = viewport.viewPosition.y
+        }
+
+        //
+        //  Implement the PropertyChangeListener
+        //
+        override fun propertyChange(e: PropertyChangeEvent) {
+            //  Keep the row table in sync with the main table
+            if ("selectionModel" == e.propertyName) {
+                setSelectionModel(main.selectionModel)
+            }
+            if ("rowHeight" == e.propertyName) {
+                repaint()
+            }
+            if ("model" == e.propertyName) {
+                main.model.addTableModelListener(this)
+                revalidate()
+            }
+        }
+
+        //
+        //  Implement the TableModelListener
+        //
+        override fun tableChanged(e: TableModelEvent?) {
+            revalidate()
+        }
+
+        /*
+	 *  Attempt to mimic the table header renderer
+	 */
+        private class RowNumberRenderer : DefaultTableCellRenderer() {
+            init {
+                horizontalAlignment = JLabel.CENTER
+            }
+
+            override fun getTableCellRendererComponent(
+                table: JTable?, value: Any?, isSelected: Boolean, hasFocus: Boolean, row: Int, column: Int
+            ): Component {
+                if (table != null) {
+                    val header: JTableHeader? = table.tableHeader
+                    if (header != null) {
+                        foreground = header.foreground
+                        background = header.background
+                        font = header.getFont()
+                    }
+                }
+                if (isSelected) {
+                    font = getFont().deriveFont(Font.BOLD)
+                }
+                text = value?.toString() ?: ""
+                border = UIManager.getBorder("TableHeader.cellBorder")
+                return this
+            }
+        }
     }
 }
--- src/test/kotlin/Main.kt
+++ src/test/kotlin/Main.kt
@@ -2,6 +2,6 @@ import bumblebee.FileManager
 import bumblebee.util.ByteViewer
 
 fun main(){
-    val imgPix = FileManager.read("src/main/resources/lenna.png")
-    val byteViewer = ByteViewer(FileManager.readBytes("src/main/resources/blackbuck.bmp"))
+    val byteArray = FileManager.readBytes("src/main/resources/lenna.png")
+    val byteViewer = ByteViewer(byteArray)
 }
