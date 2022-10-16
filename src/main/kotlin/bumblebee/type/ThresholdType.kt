84519228eaf4ffacaf43639819f254f67d79d392
volta2030 <volta2030@gmail.com>
2022-10-16 14:57:31 +0900

    -Add ThresholdType

---
+++ src/main/kotlin/bumblebee/type/ThresholdType.kt
@@ -0,0 +1,5 @@
+package bumblebee.type
+
+enum class ThresholdType {
+    OTSU, ADAPTIVE
+}
--- src/test/kotlin/Main.kt
+++ src/test/kotlin/Main.kt
@@ -4,9 +4,5 @@ import bumblebee.util.ByteViewer
 
 fun main(){
     val imgPix = FileManager.read("src/main/resources/blackbuck.bmp")
-    val hist = imgPix.toGrayScale().histogram()
-    hist.channelG.forEach{
-        println(it)
-    }
     imgPix.show()
 }
