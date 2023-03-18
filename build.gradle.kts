5ba0020bde94b3ce3ef9bca5b5bbfbd78196cde0
volta2030 <volta2030@gmail.com>
2023-03-18 21:26:28 +0900

    -Version Up

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "com.volta2030"
             artifactId = "bumblebee"
-            version = "1.0.16"
+            version = "1.0.18"
 
             from(components["java"])
         }
