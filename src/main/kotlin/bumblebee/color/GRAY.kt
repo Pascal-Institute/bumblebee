ee6c899b57d47627eafa097a20b012eb312d0d05
volta2030 <volta2030@gmail.com>
2023-09-03 22:27:36 +0900

    -change GRAY to data class

--- src/main/kotlin/bumblebee/color/GRAY.kt
+++ src/main/kotlin/bumblebee/color/GRAY.kt
@@ -2,7 +2,7 @@ package bumblebee.color
 
 import bumblebee.type.ColorType
 
-class GRAY(var g : Int) : Color {
+data class GRAY(var g : Int) : Color {
     override val colorType  = ColorType.GRAY_SCALE
     override var colorArray = arrayOf(g)
 }
